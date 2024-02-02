2e1ed0419a9788a105fded8b0abbfdff12b8ae7b
cicdtestnaveen <naveen@cicdtest.com>
2024-02-02 08:57:56 +0530

    Code Changes done for JIRA Story CT-003

---
+++ src/main/java/SeleniumSessions/GitManagementConcepts.java
@@ -0,0 +1,9 @@
+package SeleniumSessions;
+
+public class GitManagementConcepts {
+
+	public static void main(String[] args) {
+		System.out.println("This to to check the git update in feature code");
+	}
+
+}
